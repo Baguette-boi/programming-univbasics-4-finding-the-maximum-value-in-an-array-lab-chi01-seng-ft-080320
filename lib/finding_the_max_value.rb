ae98c694e6c07ff7eb87c53cea0b78676da2f306
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-16 03:57:05 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,8 +2,8 @@ def find_max_value(array)
   count = 0
   
   
-  while array[count] do
-  array[count] == 8
+  while count < array.length do
+  array[count] == 5
     count += 1
   end
   return count
